ecfeda7c5ceb19a12448a0a4f1f05e02cfa8166c
timsetsfire <tjwhittaker7642@yahoo.com>
2018-08-08 23:13:58 -0400

    update some test code

--- code/nnTest2.scala
+++ code/nnTest2.scala
@@ -1,21 +1,17 @@
-
-
-
-import scala.collection.mutable.{ArrayBuffer, Map => MutMap, Set => MutSet}
-import org.nd4j.linalg.factory.Nd4j
-import org.nd4j.linalg.api.ndarray.INDArray
-import org.nd4j.linalg.ops.transforms.Transforms.sigmoid
-import org.nd4s.Implicits._
-
 import com.github.timsetsfire.nn.node._
-import com.github.timsetsfire.nn.graph.topologicalSort
+import com.github.timsetsfire.nn.activation._
+import com.github.timsetsfire.nn.costfunctions._
 import com.github.timsetsfire.nn.optimize.GradientDescent
 import scala.util.Try
-import org.nd4j.linalg.ops.transforms.Transforms.{exp,log}
+import org.nd4j.linalg.factory.Nd4j
+import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.linalg.ops.transforms.Transforms.{sigmoid,exp,log}
+import org.nd4s.Implicits._
+import scala.collection.mutable.{ArrayBuffer, Map=>MutMap, Set=>MutSet}
 
 implicit val graph = MutMap[Node, ArrayBuffer[Node]]()
 
-
+:load code/Graph.scala
 
 import org.nd4j.linalg.ops.transforms.Transforms.{sigmoid, tanh, relu, log}
 // val temp = ((y_ * log(yhat_))) + ((y_.sub(1).mul(-1))*log(yhat_.sub(1).mul(-1)))
@@ -36,15 +32,20 @@ val y_ = Nd4j.readNumpy("resources/y.csv", ",")
  val y2_ = Nd4j.concat(1, y_, y_.mul(-1).add(1));
 
   val x = new Input()
+  x.setName("features")
   val y = new Input()
-  val h1 = Tanh(x, (2, 16), "xavier")
-  val h2 = Tanh(h1, (16, 8), "xavier")
-  val yhat = Linear(h2, (8, 2), "xavier")
-  val mse = new CrossEntropyWithLogits(y,yhat)
+  y.setName("labels")
+  val h1 = ReLU(x, (2, 16))
+  h1.setName("hidden_layer1")
+  val h2 = ReLU(h1, (16, 8))
+  h2.setName("hidden_layer2")
+  val yhat = Linear(h2, (8, 2))
+  yhat.setName("logits")
+  val ce = new CrossEntropyWithLogits(y,yhat)
 
-
-  val dag = topologicalSort(graph)
-  val learningRate = 0.01
+  val network = buildGraph(ce)
+  val dag = topologicalSort(network)
+  val learningRate = 0.1
 
 
   val xrows = x_.shape.apply(0)
@@ -60,7 +61,7 @@ val y_ = Nd4j.readNumpy("resources/y.csv", ",")
 
 
   dag.foreach( node =>
-  	if(node.getClass.getName.endsWith("Variable")) {
+  	if(node.getClass.getSimpleName == "Variable" ) {
   		val (m,n) = node.size
   		node.value = Nd4j.randn(m.asInstanceOf[Int], n.asInstanceOf[Int])*0.1
   	}
@@ -77,18 +78,18 @@ val y_ = Nd4j.readNumpy("resources/y.csv", ",")
  for(i <- 0 to 5000) {
   dag.foreach( _.forward())
   dag.reverse.map{ i => (i, Try(i.backward()))}
-  val trainables = dag.filter{ _.getClass.getName.endsWith("Variable")}
+  val trainables = dag.filter{ _.getClass.getSimpleName == "Variable" }
   for(t <- trainables) {
     val partial = t.gradients(t)
     t.value.subi(  partial * learningRate )
   }
-if(i % 100 == 0) println(s"loss: ${mse.value}")
+if(i % 100 == 0) println(s"loss: ${ce.value}")
 }
 
 val p = exp(yhat.value)
 p.diviColumnVector(p.sum(1))
-val yhat_ = Nd4j.argMax(p)
-Nd4j.argMax(y2_, 1) eq yhat_
+val yhat_ = Nd4j.argMax(p,1)
+println(s"accuracy ${(Nd4j.argMax(y.value, 1) eq yhat_).sum(0) / 600d}")
 
 /** dag.foreach( node =>
 if(node.getClass.getName.endsWith("Variable")) {
