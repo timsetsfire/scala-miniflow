0c2be7dd2b7a3843e532555263cc21952c3dcb38
Nadejda <nadejda@Nadejdas-MBP.fios-router.home>
2018-08-14 22:58:45 -0400

    add Adam optimizer

--- src/main/scala/Optimizer.scala
+++ src/main/scala/Optimizer.scala
@@ -3,8 +3,10 @@ package com.github.timsetsfire.nn.optimize
 
 import scala.collection.mutable.ArrayBuffer
 import com.github.timsetsfire.nn.node._
+import org.nd4j.linalg.factory.Nd4j
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4s.Implicits._
+import org.nd4j.linalg.ops.transforms.Transforms._
 
 
 
@@ -26,3 +28,29 @@ class GradientDescent(graph: ArrayBuffer[Node], learningRate: Double = 0.01, bet
     }
   }
 }
+
+class Adam(graph: ArrayBuffer[Node], stepSize: Double = 0.001,
+  beta1: Double = 0.9, beta2: Double = 0.999, delta: Double = 1e-8
+) {
+  val trainables = graph.filter{ _.getClass.getSimpleName == "Variable"}
+  // add a and train = true
+  val firstMoment = trainables.map{ i => (i, Nd4j.zerosLike(i.value))}.toMap
+  val secondMoment = trainables.map{ i => (i, Nd4j.zerosLike(i.value))}.toMap
+  val t = new java.util.concurrent.atomic.AtomicInteger
+
+  def optimize(feedDict: Map[Node, Any]) = {
+    feedDict.foreach{ n => n._1.value = n._2.asInstanceOf[INDArray]}
+    graph.foreach{ _.forward() }
+    graph.reverse.foreach{ _.backward() }
+    t.addAndGet(1)
+    for( n <- trainables) {
+      firstMoment(n).muli(beta1).addi(n.gradients(n).mul(1 - beta1))
+      secondMoment(n).muli(beta2).addi( pow(n.gradients(n),2).mul(1 - beta2))
+      val fhat = firstMoment(n).div(1 - math.pow(beta1, t.get))
+      val shat = secondMoment(n).div(1 - math.pow(beta2, t.get))
+      n.value.addi( fhat.mul(-stepSize).div(sqrt(shat).add(delta)))
+    }
+  }
+
+
+  }
